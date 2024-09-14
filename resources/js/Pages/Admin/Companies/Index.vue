042fe70f0c5e6a92babcd07b97b7837f3f09a1ed
Immeraria <immeraria@yandex.ru>
2024-09-14 14:29:26 +0500

    create frontend Company Index

--- resources/js/Pages/Admin/Companies/Index.vue
+++ resources/js/Pages/Admin/Companies/Index.vue
@@ -4,6 +4,7 @@ import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
+
     <Head title="Companies" />
 
     <AuthenticatedLayout>
@@ -11,14 +12,59 @@ import { Head } from '@inertiajs/vue3';
             <a :href="route('company.create')">Добавить новую компанию</a>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">Список компаний</div>
-
-                    
+        <div class="py-6">
+            <div class="container mx-auto mt-8 max-w-screen-xl">
+                <div class="overflow-x-auto px-8">
+                    <table class="min-w-full bg-white border border-gray-300">
+                        <thead>
+                            <tr class="bg-gray-200 text-gray-700">
+                                <th class="py-2 px-4 border">Название компании</th>
+                                <th class="py-2 px-4 border">Почта</th>
+                                <th class="py-2 px-4 border">Логотип</th>
+                                <th class="py-2 px-4 border">Адрес</th>
+                                <th class="py-2 px-4 border" style="width: 258px;"></th>
+                                <!-- Пустая ячейка для кнопок -->
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="hover:bg-gray-100">
+                                <td class="py-2 px-4 border">Компания 1</td>
+                                <td class="py-2 px-4 border">email1@example.com</td>
+                                <td class="py-2 px-4 border"><img src="logo1.png" alt="Логотип компании 1"
+                                        class="h-10 w-10 object-cover" /></td>
+                                <td class="py-2 px-4 border">Адрес 1</td>
+                                <td class="py-2 px-4 border" style="width: 258px;">
+                                    <button
+                                        class="bg-blue-700 text-white px-3 py-1 rounded hover:bg-blue-800">Редактировать</button>
+                                    <button
+                                        class="bg-red-700 text-white px-3 py-1 rounded hover:bg-red-800 ml-2">Удалить</button>
+                                </td>
+                            </tr>
+                            <tr class="hover:bg-gray-100" v-for="company in companies" :key="company.id">
+                                <td class="py-2 px-4 border">{{ company.name }}</td>
+                                <td class="py-2 px-4 border">{{ company.email }}</td>
+                                <td class="py-2 px-4 border">{{ company.logo }}</td>
+                                <td class="py-2 px-4 border">{{ company.address }}</td>
+                                <td class="py-2 px-4 border" style="width: 258px;">
+                                    <button
+                                        class="bg-blue-700 text-white px-3 py-1 rounded hover:bg-blue-800">Редактировать</button>
+                                    <button
+                                        class="bg-red-700 text-white px-3 py-1 rounded hover:bg-red-800 ml-2">Удалить</button>
+                                </td>
+                            </tr>
+                            <!-- Добавьте дополнительные строки по необходимости -->
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </AuthenticatedLayout>
 </template>
+
+<script>
+export default {
+    props: {
+        companies: Array,
+    },
+};
+</script>
